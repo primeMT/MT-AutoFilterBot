374aa1e11a2fa635e85576f79b7f2e062d47c555
primeMT <supremeleader2025@gmail.com>
2025-01-07 14:16:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,8 +9,8 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', '22631801'))
-API_HASH = environ.get('API_HASH', '53bc20bfbe9eb8326aa4db6be7ee346f')
+API_ID = int(environ.get('API_ID', ''))
+API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
